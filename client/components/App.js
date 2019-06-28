1c9a7838dd10414beb2b3a4df805f4800eb08eaa
Asia Gagnon <asiajoyg@gmail.com>
2019-06-28 12:59:45 -0400

    fix: body of source is rendered without styling online

--- client/components/App.js
+++ client/components/App.js
@@ -1,42 +1,41 @@
-import React from "react"
-import axios from "axios"
-
-import DB from '../db.js'
+import React from "react";
+import axios from "axios";
+import DomTest from "../Components/domTest";
+import DB from "../db.js";
+import renderHTML from "react-render-html";
 
 class App extends React.Component {
- constructor() {
-   super();
-   this.state = {
-     parsed: '',
-     stringParsed: false
-   };
-   this.handleClick = this.handleClick.bind(this);
- }
+  constructor() {
+    super();
+    this.state = {
+      parsed: "",
+      stringParsed: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
 
- componentDidMount() {
-    axios.get("http://127.0.0.1:5984/tester/d6e5852b0ed019df0956b4deb4001b14").then(json => this.setState({parsed: json.data.linkUrl}))
- }
+  componentDidMount() {
+    axios
+      .get("http://127.0.0.1:5984/body_test/2ca86a060aeb361568fd28b4c6008e01")
+      .then(json => this.setState({ parsed: json.data.linkUrl }));
+  }
 
- handleClick() {
-   let parser = new DOMParser();
-   let doc = parser.parseFromString(this.state.parsed, 'text/html');
-   console.log('between doc and setstate', doc)
-   const bodyElements = doc.getElementsbyTagName('body')
-   const innerBody = bodyElements[0].innerHTML
-   this.setState({ parsed: innerBody, stringParsed: true });
- }
+  handleClick() {
+    this.setState({ stringParsed: true });
+  }
 
- render() {
-   return this.state.stringParsed ? (
-     <div>{this.state.parsed}</div>
-   ) : (
-     <div>
-       <button type="button" onClick={() => this.handleClick()}>
-         CLICK HERE TO RENDER
-       </button>
-     </div>
-   );
- }
+  render() {
+    return this.state.stringParsed ? (
+      <div>{renderHTML(this.state.parsed)}</div>
+    ) : (
+      <div>
+        <button type="button" onClick={() => this.handleClick()}>
+          CLICK HERE TO RENDER
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
+ 
