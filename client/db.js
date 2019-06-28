4c91f016fee11da59b5134f328cfbe289a602100
Natasha Kelly <kellynatashav@yahoo.com>
2019-06-28 14:36:13 -0400

    merge-cleanup

--- client/db.js
+++ client/db.js
@@ -4,18 +4,45 @@ export default class DB{
   constructor(name){
     // set up the remote pouchdb and local and sync
     const remotedb = new PouchDB(`http://admin:graceHopper@localhost:5984/${name}`)
+    console.log ("Remote database created Successfully.");
     this.db = new PouchDB(name)
+    console.log ("Local database created Successfully.");
     this.db.sync(remotedb, {
       live: true,
       retry: true
     })
   }
 
-  async getAllArticles(){
-    let allArticles = await this.db.allArticles({include_docs: true})
-    let articles = {}
+  // async getAllArticles(){
+  //   let allArticles = await this.db.allArticles({include_docs: true})
+  //   let articles = {}
+  //
+  //   allArticles.rows.forEach(a => articles[a.id] = a.doc)
+  //   return artices
+  // }
 
-    allArticles.rows.forEach(a => articles[a.id] = a.doc)
-    return artices
+  async addArticles(obj){
+    let addedArticle = await this.db.put(obj)
+  }
+
+  async getAllArticles(obj){
+    let allArticles = await this.db.get("id-goes-here").then(function(obj){
+      console.log(obj)
+    })
+  }
+
+  async updateAnArticle(doc){
+    // fetch mittens
+    db.get('id-goes-here').then(function (doc) {
+      // update their age
+      doc.age = 4;
+      // put them back
+      return db.put(doc);
+    }).then(function () {
+      // fetch mittens again
+      return db.get('id-goes-here');
+    }).then(function (doc) {
+      console.log(doc);
+    });
   }
 }
--- server/index.js
+++ server/index.js
@@ -3,10 +3,9 @@ const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = 5001;
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-
+// if (process.env.NODE_ENV !== 'production') require("../secret")
 
 
 // Logging middleware
