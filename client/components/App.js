dd259d0f9238bcc1b5ed27add453d2fde4edecb8
amaris chang <amaris.a.chang@gmail.com>
2019-06-30 17:30:59 -0400

    adds pouch find and more queries

--- client/components/App.js
+++ client/components/App.js
@@ -1,31 +1,39 @@
-
-import React,{Component} from "react"
-import axios from "axios"
-import renderHTML from "react-render-html";
-import DB from '../db.js'
+import React, { Component } from 'react';
+import axios from 'axios';
+import renderHTML from 'react-render-html';
+import DB from '../db.js';
 
 class App extends React.Component {
- constructor() {
-   super();
-   this.state = {
-     db: new DB("alice2"),
-     parsed: '',
-     stringParsed: false
-   };
-   this.handleClick = this.handleClick.bind(this);
- }
+  constructor() {
+    super();
+    this.state = {
+      db: new DB('alice2'),
+      parsed: '',
+      stringParsed: false,
+      articleTitle: '',
+      articleUrl: '',
+      goalId: 1,
+      userId: 2,
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleFindArticles = this.handleFindArticles.bind(this);
+  }
 
   componentDidMount() {
     // axios
     //   .get("http://127.0.0.1:5984/body_test/2ca86a060aeb361568fd28b4c6008e01")
     //   .then(json => this.setState({ parsed: json.data.linkUrl }));
-
     // this.state.db.getAllArticles("2ca86a060aeb361568fd28b4c600abd1")
   }
 
   handleClick() {
-    this.setState({ stringParsed: true });
-    this.state.db.getAllArticles("2ca86a060aeb361568fd28b4c600abd1")
+    // this.setState({ stringParsed: true });
+    this.state.db.addArticle(this.state);
+  }
+  async handleFindArticles() {
+    // this.setState({ stringParsed: true });
+    await this.state.db.createDBIndex()
+    this.state.db.findArticle(this.state.userId);
   }
 
   render() {
@@ -33,10 +41,54 @@ class App extends React.Component {
       <div>{renderHTML(this.state.parsed)}</div>
     ) : (
       <div>
-        <button type="button" onClick={() => this.handleClick()}>
-          CLICK HERE TO RENDER
+        <form>
+          Article Title:
+          <br />
+          <input
+            type="text"
+            value={this.state.articleTitle}
+            onChange={event =>
+              this.setState({ articleTitle: event.target.value })
+            }
+          />
+          <br />
+          Article Url:
+          <br />
+          <input
+            type="text"
+            value={this.state.articleUrl}
+            onChange={event =>
+              this.setState({ articleUrl: event.target.value })
+            }
+          />
+          <br />
+          Goal Id:
+          <br />
+          <input
+            type="text"
+            value={this.state.goalId}
+            onChange={event => this.setState({ goalId: event.target.value })}
+          />
+          <br />
+          <br />
+          <button
+            type="button"
+            value="Submit"
+            onClick={() => this.handleClick(this.state)}
+          >
+            Submit
+          </button>
+        </form>
+        <button
+          type="button"
+          onClick={() => this.handleFindArticles(this.state.userId)}
+        >
+          Find Articles By User
         </button>
       </div>
+      /* <button type="button" onClick={() => this.handleClick()}>
+          CLICK HERE TO RENDER
+      </button> */
     );
   }
 }
--- client/db.js
+++ client/db.js
@@ -1,18 +1,24 @@
 import PouchDB from 'pouchDB';
+import Find from 'pouchdb-find';
+PouchDB.plugin(Find);
 
-export default class DB{
-  constructor(name){
+export default class DB {
+  constructor(name) {
     // set up the remote pouchdb and local and sync
-    const remotedb = new PouchDB(`http://admin:graceHopper@localhost:5984/${name}`)
-    console.log ("Remote database created Successfully.");
+    const remotedb = new PouchDB(
+      `http://admin:graceHopper@localhost:5984/${name}`
+    );
+    console.log('Remote database created Successfully.');
     this.db = new PouchDB(name, { skip_setup: true });
-    console.log ("Local database created Successfully.");
+    console.log('Local database created Successfully.');
     this.db.sync(remotedb, {
       live: true,
-      retry: true
-    })
+      retry: true,
+    });
   }
-
+  async createDBIndex() {
+    this.db.createIndex({ index: { fields: ['userId'] } });
+  }
   // async getAllArticles(){
   //   let allArticles = await this.db.allArticles({include_docs: true})
   //   let articles = {}
@@ -21,28 +27,86 @@ export default class DB{
   //   return artices
   // }
 
-  async addArticles(obj){
-    let addedArticle = await this.db.put(obj)
-  }
-
-  async getAllArticles(id){
-    let allArticles = await this.db.get(id).then(function(doc){
-      console.log(doc)
-    })
-  }
-
-  async updateAnArticle(doc){
-    // fetch mittens
-    db.get('id-goes-here').then(function (doc) {
-      // update their age
-      doc.age = 4;
-      // put them back
-      return db.put(doc);
-    }).then(function () {
-      // fetch mittens again
-      return db.get('id-goes-here');
-    }).then(function (doc) {
+  async getArticlesByUser(userId) {
+    let allArticles = await this.db.get(id).then(function(doc) {
       console.log(doc);
     });
+  }
+
+  async addArticles(obj) {
+    let addedArticle = await this.db.put(obj);
+  }
+
+  async getAllArticles(id) {
+    let allArticles = await this.db.get(id).then(function(doc) {
+      console.log(doc);
+    });
+  }
+
+  async addUser(obj) {
+    let user = {
+      _id: new Date().toISOString(),
+      email: obj.email,
+      password: obj.password,
+      // goals: {
+      //   goalId: obj.goal.id,
+      //   title: obj.goal.title,
+      //   articles: [].concat(obj.goals.articleUrl),
+      //   completed: false
+      // }
+    };
+    this.db.put(user).then(function(err, result) {
+      if (!err) {
+        console.log('Successfully added a user!');
+      }
+    });
+  }
+
+  async addArticle(obj) {
+    let newArticle = {
+      _id: new Date().toISOString(),
+      articleTitle: obj.articleTitle,
+      articleUrl: obj.articleUrl,
+      userId: obj.userId,
+      goalId: obj.goalId,
+    };
+    console.log('the object being put into db', newArticle);
+    this.db.put(newArticle).then(function(err, result) {
+      if (!err) {
+        console.log('Successfully added a article!');
+      } else {
+        console.log(err);
+      }
+    });
+  }
+
+  async updateAnArticle(doc) {
+    // fetch mittens
+    db.get('id-goes-here')
+      .then(function(doc) {
+        // update their age
+        doc.age = 4;
+        // put them back
+        return db.put(doc);
+      })
+      .then(function() {
+        // fetch mittens again
+        return db.get('id-goes-here');
+      })
+      .then(function(doc) {
+        console.log(doc);
+      });
+  }
+
+  async findArticle(inputUserId) {
+    this.db
+      .find({ selector: { userId: inputUserId } })
+      .then(function(err, result) {
+        if (!err) {
+          console.log('successfully found articles by user');
+        } else {
+          console.log(err);
+        }
+      });
   }
 }
