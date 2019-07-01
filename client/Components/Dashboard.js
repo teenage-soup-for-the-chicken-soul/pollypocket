97c4345e6ae49171f81e0336b560f8b53d604a7f
Asia Gagnon <asiajoyg@gmail.com>
2019-07-01 10:24:09 -0400

    fix: (comp/dashboard) option for dashboard with grid list and chip navbar and button

--- client/Components/Dashboard.js
+++ client/Components/Dashboard.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Avatar from '@material-ui/core/Avatar';
-import Chip from '@material-ui/core/Chip';
-import FaceIcon from '@material-ui/icons/Face';
-import DoneIcon from '@material-ui/icons/Done';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Avatar from "@material-ui/core/Avatar";
+import Chip from "@material-ui/core/Chip";
+import FaceIcon from "@material-ui/icons/Face";
+import DoneIcon from "@material-ui/icons/Done";
 
 const useStyles = makeStyles(theme => ({
   // root: {
@@ -12,95 +12,81 @@ const useStyles = makeStyles(theme => ({
   //   flexWrap: 'wrap',
   // },
   chip: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(1)
   },
   root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    backgroundColor: theme.palette.background.paper
   },
   gridList: {
-    flexWrap: 'nowrap',
+    flexWrap: "nowrap",
     // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: 'translateZ(0)',
+    transform: "translateZ(0)"
   },
   title: {
-    color: theme.palette.primary.light,
+    color: theme.palette.primary.light
   },
   titleBar: {
     background:
-      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-  },
+      "linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)"
+  }
 }));
 // import {someThunk} from 'somewhere'
 // import {me} from '../store/user' => or our version of it
 const classes = useStyles();
-class Dashboard extends React.Component{
+class Dashboard extends React.Component {
+  handleDelete() {}
 
-  handleDelete(){
+  handleClick() {}
 
-  }
-
-  handleClick(){
-
-  }
-
-  render(){
+  render() {
     if (this.props.loading) {
-      return <div>loading...</div>
+      return <div>loading...</div>;
     }
     return (
-      <div id='container'>
-<div id='sidebar'>
-   {goalList.map(goal=> {
-        <Chip
-
-        label="goal.name"
-        onClick={handleclick}
-        onDelete={handleDelete}
-        className={classes.chip}
-      />
-   })}
-      </div>
-      {goalList.map(goal => (
-      <div className={classes.root}>
-      <GridList className={classes.gridList} cols={2.5}>
-        {articles.map(article => (
- <GridListTile key={article.img}>
- <img src={article.img} alt={article.title} />
- <GridListTileBar
-   title={article.title}
-   classes={{
-     root: classes.titleBar,
-     title: classes.title,
-   }}
-   actionIcon={
-     <IconButton aria-label={`star ${article.title}`}>
-       <StarBorderIcon className={classes.title} />
-     </IconButton>
-   }
- />
-</GridListTile>
+      <div id="container">
+        <div id="sidebar">
+          {goalList.map(goal => {
+            <Chip
+              label="goal.name"
+              onClick={handleclick}
+              onDelete={handleDelete}
+              className={classes.chip}
+            />;
+          })}
+        <button type='button'>Add Article</button>
+        </div>
+        {goalList.map(goal => (
+          <div className={classes.root}>
+            <GridList className={classes.gridList} cols={2.5}>
+              {articles.map(article => (
+                <GridListTile key={article.img}>
+                  <img src={article.img} alt={article.title} />
+                  <GridListTileBar
+                    title={article.title}
+                    classes={{
+                      root: classes.titleBar,
+                      title: classes.title
+                    }}
+                    actionIcon={
+                      <IconButton aria-label={`star ${article.title}`}>
+                        <StarBorderIcon className={classes.title} />
+                      </IconButton>
+                    }
+                  />
+                </GridListTile>
+              ))}
+            </GridList>
+          </div>
         ))}
-      </GridList>
-    </div>
-      ))}
 
-      <div id='goalOneList'>
-
+        <div id="goalOneList" />
+        <div id="goalTwoList" />
+        <div id="goalThreeList" />
       </div>
-      <div id='goalTwoList'>
-
-</div>
-<div id='goalThreeList'>
-
-</div>
-      </div>
-
-        );
+    );
   }
 }
-
-
