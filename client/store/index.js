e9207fa3119dc9112429c86b737872aa173d620b
Natasha Kelly <kellynatashav@yahoo.com>
2019-07-02 10:55:58 -0400

    feat(redux/pouch) - added module for persisting parts of redux to pouch

--- client/store/index.js
+++ client/store/index.js
@@ -5,11 +5,24 @@ import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './reducers/users'
 import articles from './reducers/articles'
 
+import { persistentStore } from 'redux-pouchdb';
+const db = new PouchDB('articles');
+
+
 const reducer = combineReducers({user,articles})
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
+  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}, persistentStore(db)))
 )
 const store = createStore(reducer, middleware)
 
 export default store
 export * from './reducers/users'
+
+
+/*
+What is going on here?
+It is very simple:
+The PouchDB database persists the state of chosen parts of the Redux store every time it changes.
+Your reducers will be passed the state from PouchDB when your app loads and every time a change arrives (if you are syncing with a remote db).
+
+*/
