5016e6facaeb00f890c38200a3e896c7727b0918
amaris chang <amaris.a.chang@gmail.com>
2019-07-10 13:55:45 -0400

    [test]google oauth takes out secrets

--- server/auth/google.js
+++ server/auth/google.js
@@ -1,9 +1,8 @@
-const passport = require('passport')
-const router = require('express').Router()
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
-const {User} = require('../db/models')
-require("../../secrets")
-module.exports = router
+const passport = require('passport');
+const router = require('express').Router();
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const { User } = require('../db/models');
+module.exports = router;
 
 /**
  * For OAuth keys and other secrets, your Node process will search
@@ -20,38 +19,38 @@ module.exports = router
  */
 
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-  console.log('Google client ID / secret not found. Skipping Google OAuth.')
+  console.log('Google client ID / secret not found. Skipping Google OAuth.');
 } else {
   const googleConfig = {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
-  }
+    callbackURL: process.env.GOOGLE_CALLBACK,
+  };
 
   const strategy = new GoogleStrategy(
     googleConfig,
     (token, refreshToken, profile, done) => {
-      const googleId = profile.id
-      const email = profile.emails[0].value
+      const googleId = profile.id;
+      const email = profile.emails[0].value;
 
       User.findOrCreate({
-        where: {googleId},
-        defaults: {email}
+        where: { googleId },
+        defaults: { email },
       })
         .then(([user]) => done(null, user))
-        .catch(done)
+        .catch(done);
     }
-  )
+  );
 
-  passport.use(strategy)
+  passport.use(strategy);
 
-  router.get('/', passport.authenticate('google', {scope: 'email'}))
+  router.get('/', passport.authenticate('google', { scope: 'email' }));
 
   router.get(
     '/callback',
     passport.authenticate('google', {
       successRedirect: '/home',
-      failureRedirect: '/login'
+      failureRedirect: '/login',
     })
-  )
+  );
 }
